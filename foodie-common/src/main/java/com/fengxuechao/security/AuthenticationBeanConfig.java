da6adb27e6fe3e0f113894aa3b065dd9fb0d0d8f
littlefxc <12741723+littlefxc@users.noreply.github.com>
2020-03-19 22:41:44 +0800

    feat(foodie-pojo):添加更安全的密码工具类

---
+++ foodie-common/src/main/java/com/fengxuechao/security/AuthenticationBeanConfig.java
@@ -0,0 +1,41 @@
+package com.fengxuechao.security;
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.DelegatingPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.crypto.password.Pbkdf2PasswordEncoder;
+import org.springframework.security.crypto.scrypt.SCryptPasswordEncoder;
+import org.springframework.stereotype.Component;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 认证相关的扩展点配置。配置在这里的bean，业务系统都可以通过声明同类型或同名的bean来覆盖安全
+ * 模块默认的配置。
+ *
+ * @author fengxuechao
+ * @date 2020/3/19
+ */
+@Component
+public class AuthenticationBeanConfig {
+
+    /**
+     * 默认密码处理器
+     *
+     * @return 密码处理器
+     */
+    @Bean
+    @ConditionalOnMissingBean(PasswordEncoder.class)
+    public PasswordEncoder passwordEncoder() {
+        String encodingId = "bcrypt";
+        Map<String, PasswordEncoder> encoders = new HashMap<>();
+        encoders.put(encodingId, new BCryptPasswordEncoder());
+        encoders.put("pbkdf2", new Pbkdf2PasswordEncoder());
+        encoders.put("scrypt", new SCryptPasswordEncoder());
+
+        return new DelegatingPasswordEncoder(encodingId, encoders);
+    }
+}
--- foodie-service/src/main/java/com/fengxuechao/service/UserService.java
+++ foodie-service/src/main/java/com/fengxuechao/service/UserService.java
@@ -7,16 +7,26 @@ public interface UserService {
 
     /**
      * 判断用户名是否存在
+     *
+     * @param username 用户名
+     * @return 是否存在
      */
     boolean queryUsernameIsExist(String username);
 
     /**
      * 判断用户名是否存在
+     *
+     * @param userBO 用于创建用户的信息
+     * @return 创建成功的用户信息
      */
     Users createUser(UserBO userBO);
 
     /**
      * 检索用户名和密码是否匹配，用于登录
+     *
+     * @param username 用户名
+     * @param password 密码
+     * @return 用户
      */
     Users queryUserForLogin(String username, String password);
 }
