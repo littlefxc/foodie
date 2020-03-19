85a4581a2e567a76426d5c92944f1d1e3db88853
littlefxc <12741723+littlefxc@users.noreply.github.com>
2020-03-19 23:05:00 +0800

    test(foodie):测试密码加密器

--- foodie-common/src/test/java/com/fengxuechao/security/AuthenticationBeanConfigTest.java
+++ foodie-common/src/test/java/com/fengxuechao/security/AuthenticationBeanConfigTest.java
@@ -1,42 +1,37 @@
 package com.fengxuechao.security;
 
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
-import org.springframework.context.annotation.Bean;
+import org.junit.Before;
+import org.junit.Test;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.DelegatingPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.crypto.password.Pbkdf2PasswordEncoder;
 import org.springframework.security.crypto.scrypt.SCryptPasswordEncoder;
-import org.springframework.stereotype.Component;
 
 import java.util.HashMap;
 import java.util.Map;
 
 /**
- * 认证相关的扩展点配置。配置在这里的bean，业务系统都可以通过声明同类型或同名的bean来覆盖安全
- * 模块默认的配置。
- *
  * @author fengxuechao
  * @date 2020/3/19
  */
-@Component
-public class AuthenticationBeanConfig {
+public class AuthenticationBeanConfigTest {
 
-    /**
-     * 默认密码处理器
-     * <p>
-     * 默认编码器是 bcrypt
-     *
-     * @return 密码处理器
-     */
-    @Bean
-    @ConditionalOnMissingBean(PasswordEncoder.class)
-    public PasswordEncoder passwordEncoder() {
+    private PasswordEncoder passwordEncoder;
+
+    @Before
+    public void setUp() throws Exception {
         String encodingId = "bcrypt";
         Map<String, PasswordEncoder> encoders = new HashMap<>();
         encoders.put(encodingId, new BCryptPasswordEncoder());
         encoders.put("pbkdf2", new Pbkdf2PasswordEncoder());
         encoders.put("scrypt", new SCryptPasswordEncoder());
-        return new DelegatingPasswordEncoder(encodingId, encoders);
+        passwordEncoder = new DelegatingPasswordEncoder(encodingId, encoders);
+    }
+
+    @Test
+    public void bcrypt() {
+        String encodePassword = passwordEncoder.encode("123456");
+        System.out.println(encodePassword);
     }
 }
