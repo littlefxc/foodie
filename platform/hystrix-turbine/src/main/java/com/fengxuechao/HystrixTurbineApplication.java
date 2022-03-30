6758c1840e8a0298ee5a9ad0f4d451a256cc68f5
littlefxc <12741723+littlefxc@users.noreply.github.com>
2022-03-30 14:21:06 +0800

    refactor(platform/hystrix-turbine): 移除无用代码

--- platform/hystrix-turbine/src/main/java/com/fengxuechao/HystrixTurbineApplication.java
+++ platform/hystrix-turbine/src/main/java/com/fengxuechao/HystrixTurbineApplication.java
@@ -1,14 +1,11 @@
 package com.fengxuechao;
 
-import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.netflix.turbine.EnableTurbine;
-import org.springframework.context.annotation.Bean;
 
 @EnableDiscoveryClient
 @EnableHystrixDashboard
@@ -16,16 +13,6 @@ import org.springframework.context.annotation.Bean;
 @EnableTurbine
 @SpringBootApplication
 public class HystrixTurbineApplication {
-
-    /*@Bean
-    public ServletRegistrationBean getServlet() {
-        HystrixMetricsStreamServlet streamServlet = new HystrixMetricsStreamServlet();
-        ServletRegistrationBean registrationBean = new ServletRegistrationBean(streamServlet);
-        registrationBean.setLoadOnStartup(1);
-        registrationBean.addUrlMappings("/actuator/hystrix.stream");
-        registrationBean.setName("HystrixMetricsStreamServlet");
-        return registrationBean;
-    }*/
 
     public static void main(String[] args) {
         SpringApplication.run(HystrixTurbineApplication.class, args);
